baa6c04857f49a0e073bd04634d06284d98aa250
laughingturtle <monkeydo@Monkeys-MacBook-Pro.local>
2019-02-25 20:08:22 -0800

    env vars + new local branch

--- db/db.js
+++ db/db.js
@@ -1,22 +1,14 @@
 const { Client } = require('pg');
 
-/* local */
+/* EC2 */
 const connection = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'twitchchat',
-  password: 'fish22',
-  port: 5432,
+  user: process.env.PSQL_USERNAME,
+  host: process.env.PSQL_HOSTNAME,
+  database: process.env.PSQL_DB_NAME,
+  password: process.env.PSQL_PASSWORD,
+  port: process.env.PSQL_PORT,
 });
 
-/* EC2 */
-// const connection = new Client({
-//   user: process.env.PSQL_USERNAME,
-//   host: process.env.PSQL_HOSTNAME,
-//   database: process.env.PSQL_DB_NAME,
-//   password: process.env.PSQL_PASSWORD,
-//   port: process.env.PSQL_PORT,
-// });
 connection.connect();
 
 module.exports.connection = connection;
