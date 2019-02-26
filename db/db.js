d7058f763e7b9419cb2bd1cbec2c31ecbbefbc5e
laughingturtle <monkeydo@Monkeys-MacBook-Pro.local>
2019-02-25 20:05:34 -0800

    adding environment variables

--- db/db.js
+++ db/db.js
@@ -1,5 +1,6 @@
 const { Client } = require('pg');
 
+/* local */
 const connection = new Client({
   user: 'postgres',
   host: 'localhost',
@@ -7,6 +8,15 @@ const connection = new Client({
   password: 'fish22',
   port: 5432,
 });
+
+/* EC2 */
+// const connection = new Client({
+//   user: process.env.PSQL_USERNAME,
+//   host: process.env.PSQL_HOSTNAME,
+//   database: process.env.PSQL_DB_NAME,
+//   password: process.env.PSQL_PASSWORD,
+//   port: process.env.PSQL_PORT,
+// });
 connection.connect();
 
 module.exports.connection = connection;
