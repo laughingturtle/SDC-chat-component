40d9ddfd625078c17e8df0dc4e7a16fa8b62e5ef
laughingturtle <monkeydo@Monkeys-MacBook-Pro.local>
2019-03-11 10:51:00 -0700

    refining production server

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { ChatContainer } from './components/ChatContainer.jsx';
-
-ReactDOM.render(<ChatContainer />, document.getElementById('main'));
